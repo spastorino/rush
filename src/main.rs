2d7b3f75288cbc87984bcea86c7072cdb78199bb
Kalil de Lima <kaozdl@gmail.com>
2019-05-14 22:57:43 -0300

    add support for multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use std::io::{self, Write};
+use std::str::SplitWhitespace;
 use std::process::Command;
+
+struct Cmd<'a> {
+    comm : &'a str,
+    args : SplitWhitespace<'a>,
+}
+
+impl<'a> Cmd<'a> {
+    fn parse(input : &'a str) -> Vec<Cmd<'a>> {
+        input.split(";").map(|cmd| {
+            let mut parsed_cmd = cmd.split_whitespace();
+            Cmd {
+                comm: parsed_cmd.next().unwrap(),
+                args: parsed_cmd,
+            }
+        }).collect()
+    }
+}
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
