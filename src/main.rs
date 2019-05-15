1f2c89ca72a82ad4d4c48cd04480eb67c3fd6a0a
Santiago Pastorino <spastorino@gmail.com>
2019-05-14 21:11:28 -0300

    Initial REPL

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io::{self, Write};
+use std::process::Command;
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut stderr = io::stderr();
+
+    loop {
+        stdout.write(b"> ")?;
+        stdout.flush()?;
+
+        let mut input = String::new();
+        stdin.read_line(&mut input)?;
+        let mut args = input.split_whitespace();
+
+        if let Some(cmd) = args.next() {
+            match Command::new(cmd).args(args).spawn() {
+                Ok(mut child) => {
+                    child.wait()?;
+                }
+                Err(_) => {
+                    stderr.write(b"Command not found\n")?;
+                }
+            }
+        }
+    }
+}
